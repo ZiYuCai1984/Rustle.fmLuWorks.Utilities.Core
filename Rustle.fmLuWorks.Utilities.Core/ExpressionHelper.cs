dc5fdf1f54eadc4a37bf66e22a83f4415b092f21
zyCai <yucaizi1984@gmail.com>
2020-08-01 20:49:14 +0800

    add action file

---
+++ Rustle.fmLuWorks.Utilities.Core/ExpressionHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Rustle.fmLuWorks.Utilities.Core
+{
+    public static class ExpressionHelper
+    {
+        public static T GetPropertyValue<T>(this Expression<Func<T>> expression)
+        {
+            return expression.Compile().Invoke();
+        }
+
+        public static void SetPropertyValue<T>(this Expression<Func<T>> expression, T value)
+        {
+            if (!(expression.Body is MemberExpression memberExpression))
+            {
+                throw new ArgumentException(nameof(memberExpression));
+            }
+
+            if (!(memberExpression.Member is PropertyInfo propertyInfo))
+            {
+                throw new ArgumentException(nameof(propertyInfo));
+            }
+
+            var target = Expression.Lambda(memberExpression.Expression)
+                .Compile()
+                .DynamicInvoke();
+
+            propertyInfo.SetValue(target, value);
+        }
+
+        public static Expression<T> CreateExpression<T>(Expression<T> expression)
+        {
+            return expression;
+        }
+    }
+}
---
+++ Rustle.fmLuWorks.Utilities.Core/ReflectionHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedType.Global
+
+namespace Rustle.fmLuWorks.Utilities.Core
+{
+    public static class ReflectionHelper
+    {
+        private static void SetGetOnlyProperty(
+            object obj,
+            string propertyName,
+            object newValue)
+        {
+            var type = obj.GetType();
+            var field = type
+                .GetRuntimeFields()
+                .FirstOrDefault(
+                    a => Regex.IsMatch(
+                        a.Name,
+                        $"\\A<{propertyName}>k__BackingField\\Z"));
+            Debug.Assert(field != null);
+            field.SetValue(obj, newValue);
+        }
+
+        public static IEnumerable<T> GetPrivateInstanceValues<T>(
+        this object obj,
+           Predicate<T>? predicate = null)
+        {
+            return from v in (from p in
+                        obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.NonPublic)
+                              where p.PropertyType == typeof(T)
+                              select (T)p.GetValue(obj, null)).ToArray()
+                   where predicate == null || predicate.Invoke(v)
+                   select v;
+        }
+
+
+        public static T GetPrivateInstanceValue<T>(
+            this object obj,
+            Predicate<T>? predicate = null)
+        {
+            return obj.GetPrivateInstanceValues(predicate).FirstOrDefault();
+        }
+    }
+}
